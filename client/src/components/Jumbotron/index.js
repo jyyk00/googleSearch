29abf6f3966ffd23cde851ab11c9a091cead0b3d
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-10 16:39:32 -0700

    new title

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 function Jumbotron() {
     return (
       <div className="jumbotron text-center">
-        <h1><span><a className="white" id="jumboHead" href="/">My Virtual Bookshelf</a></span></h1>
+        <h1><span><a className="white" id="jumboHead" href="/">libraryWeb</a></span></h1>
           <div>
             <a className="white" target="_blank" rel="noopener noreferrer" href="http://developers.google.com/books/">
               Powered by Google Books
