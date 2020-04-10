45ce3c2cdd6501dd50f64a9f27cc8d997d731470
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-10 16:55:22 -0700

    state search

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -5,3 +5,15 @@ import AddBookBtn from "../../components/AddBookBtn";
 import { Row, Col } from "../../components/Grid";
 import { BookList, BookListItem } from "../../components/BookList";
 import EmptyList from "../../components/EmptyList";
+
+class Search extends Component {
+    state = {
+      searchRes: [],
+      query: "",
+      books: []
+    };
+  
+    displayRes = data => {
+      this.setState({ books: data.items });
+    };
+  
