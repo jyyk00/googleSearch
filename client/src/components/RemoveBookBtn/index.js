837d1818ac0112eeeecc9159cc606902730405c5
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-10 16:33:29 -0700

    function for button deletion

--- client/src/components/RemoveBookBtn/index.js
+++ client/src/components/RemoveBookBtn/index.js
@@ -1,2 +1,10 @@
 import React from "react";
 import Button from "../Button";
+
+function RemoveBookBtn(props) {
+    return (
+      <Button type="danger" className="delete-btn" {...props} role="button" tabIndex="0">
+        Delete
+      </Button>
+    );
+  }
