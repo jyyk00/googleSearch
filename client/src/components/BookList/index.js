64746fed80736bf3c0645507edd193a02184182d
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-10 16:12:07 -0700

    rendering book list item from bootstrap

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,3 +1,9 @@
 import React from "react";
 import Thumbnail from "../Thumbnail";
 import { Container, Row, Col } from "../Grid";
+
+export function BookList({children}) {
+    return (
+      <ul className="list-group">{children}</ul>
+      );
+  }
