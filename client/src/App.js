ceb0e887c9de03ee22667a545e172ecf234f12ab
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-10 16:58:15 -0700

    new comp func

--- client/src/App.js
+++ client/src/App.js
@@ -8,25 +8,36 @@ import { ToastContainer, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav/>
+        <Router>
+          <Container>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            transition={Zoom}
+            hideProgressBar
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnVisibilityChange
+            draggable={false}
+            pauseOnHover
+          />
+            <Switch>
+              <Route exact path="/" component={Search} />
+              <Route path="/bookshelf" component={Saved} />
+              <Route exact path="/books/:id" component={null} />
+              <Route component={null} />
+            </Switch>
+          </Container>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
