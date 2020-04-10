17444323aacc293bf9ece3f574bcb476b762f1e9
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-10 16:06:29 -0700

    work in add button

--- client/src/components/AddBookBtn/index.js
+++ client/src/components/AddBookBtn/index.js
@@ -4,3 +4,15 @@ import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 //toast for notifications //make sure you run the install if not in package already
 import { toast } from 'react-toastify';
+
+class AddBookBtn extends React.Component{
+ 
+    postToDB = (book) => {
+        var dbBook = {
+          title: book.title,
+          authors: book.authors,
+          synopsis: book.synopsis,
+          thumbnail: book.thumbnail,
+          link: book.link
+        }
+    
