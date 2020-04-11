4699bb0c9c89dcfef72be030d97c496883094863
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-10 17:34:25 -0700

    new styling for jumbotron

---
+++ client/build/precache-manifest.8fe6422a547ec29dc38989db489ab5f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3481ab40b336322e652b39f5bc915405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7175feab71d62ffe2ab1",
+    "url": "/static/css/2.9bde7c98.chunk.css"
+  },
+  {
+    "revision": "bf1294befef988d9199c",
+    "url": "/static/css/main.ebf3bfc2.chunk.css"
+  },
+  {
+    "revision": "7175feab71d62ffe2ab1",
+    "url": "/static/js/2.42dd6882.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.42dd6882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1294befef988d9199c",
+    "url": "/static/js/main.ed18984f.chunk.js"
+  },
+  {
+    "revision": "25fcbcbb01cbd63d189d",
+    "url": "/static/js/runtime-main.c1819bd0.js"
+  }
+]);
