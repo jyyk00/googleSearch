b04106e7eadce2e195f42a8dc0c0c45772216bbb
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-10 17:05:02 -0700

    route forid

--- routes/api/books.js
+++ routes/api/books.js
@@ -4,3 +4,11 @@ const booksController = require("../../controllers/booksController");
 router.route("/")
   .get(booksController.findAll)
   .post(booksController.create);
+
+  router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
+
+module.exports = router;
